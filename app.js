fc05e732157e80378d425401055f5aa284153108
Mashtar89 <mashtar89@users.noreply.github.com>
2022-03-28 19:06:06 -0400

    main

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const _ = require('lodash')
+const EventEmitter = require('events');
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', () => {
+    console.log(`data recieved`)
+})
+
+customEmitter.emit('response')
